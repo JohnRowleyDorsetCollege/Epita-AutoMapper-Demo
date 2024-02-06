4b2f340d4bbc5234449eb3419f20a525c5bb6310
johnrowley <john.rowley@dorset.ie>
2024-02-06 10:04:37 +0000

    details added

--- Services/SuperHeroRepository.cs
+++ Services/SuperHeroRepository.cs
@@ -13,7 +13,34 @@ namespace Epita_AutoMapper_Demo.Services
         public static List<Superhero> SuperHeroes()
         {
 
-            return new List<Superhero>();
+            return new List<Superhero>() {
+
+
+             new Superhero {
+         Id = 1,
+         Name ="Spider Man",
+         FirstName = "Peter",
+         LastName = "Parker",
+         Universe = "New York City",
+         dateAdded = new DateTime(2001, 08, 10),
+         dateUpdated = null
+     },
+     new Superhero {
+         Id = 2,
+         Name ="Iron Man",
+         FirstName = "Tony",
+         LastName = "Stark",
+         Universe = "Malibu",
+         dateAdded = new DateTime(1970, 05, 29),
+         dateUpdated = null
+     }
+
+
+
+
+
+
+            };
         }
     }
 
